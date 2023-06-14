e75d184455872822f542321e2948d7cd53710fc8
Jose Jimenez <Anthonyjimenez1120@gmail.com>
2023-06-14 00:33:27 -0400

    segundo commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log("hola mundo!")
+
+const array = ["pepe","Jose","pedro","Carlos"]
+
+const recorrerArray = (array)=>{
+    for(let i = 0; array > i; i++){
+       return console.log(array[i])
+    }
+}
+
+recorrerArray(array)
